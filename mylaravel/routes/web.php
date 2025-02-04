5389f7b7f54dc9f6f9b5955a05d7e54a0b91a734
Raveroj <Raverojsonthi47@gmail.com>
2025-02-04 15:10:09 +0700

    edit add delete

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,18 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Mycontroller;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
-Route::get('/mycontroller/{id?}', [Mycontroller::class, 'myfunction']);
-Route::post('/mycontroller/{id?}', [Mycontroller::class, 'myfunction']);
 
-Route::get('/login',
-[App\Http\Controllers\LoginController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function () {
+    return "<h1>Hello world</h1>";
+});
+Route::get("/mycontroller/{id?}",
+[Mycontroller::class,'myfunction']);
 
-Route::get('/home',
-[App\Http\Controllers\HomeController::class, 'index']);
+Route::post('/mycontroller/{id?}',
+[Mycontroller::class,'myfunction']);
 
-Route::get('/register',
-[App\Http\Controllers\RegisterController::class, 'index']);
+Route ::get('/',function(){
+    return view('layouts.default');
+});
 
-Route::get('/',
-[App\Http\Controllers\HomeController::class, 'index']);
+Route ::get('/home',function(){
+    return view('home');
+});
+
+Route ::get('/login',
+[LoginController::class,'index']);
+
+Route ::get('/register',
+[RegisterController::class,'register']);
+
+Route ::post('/register',
+[RegisterController::class,'create']);
+
+Route ::get('/user',
+[UserController::class,'index']);
+
+Route ::get('/user/{id}',
+[UserController::class,'edit']);
+
+Route ::put ('/user',
+[UserController::class,'saveEdit']);
+
+Route ::delete ('/user',
+[UserController::class,'delete']);
